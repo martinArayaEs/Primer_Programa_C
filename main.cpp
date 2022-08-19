0f4f630c3431b2f79ca328aed9af27d0ceb8f3ce
martinArayaEs <martin.araya.es@gmail.com>
2022-08-19 18:02:45 -0400

    Operaciones aritmeticas basicas: suma, resta, mult y div

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n1, n2, suma = 0, resta = 0, mult = 0, div = 0;
+
+    cout<<"Digite un numero: ";cin>>n1;
+    cout<<"Digite otro numero: ";cin>>n2;
+
+    suma = n1 + n2;
+    resta = n1 - n2;
+    mult = n1 * n2;
+    div = n1 / n2;
+
+    cout<<"\n\nLa suma es: "<<suma;
+    cout<<"\nLa resta es: "<<resta;
+    cout<<"\nLa multiplicacion es: "<<mult;
+    cout<<"\nLa division es: "<<div;
+
     return 0;
 }
